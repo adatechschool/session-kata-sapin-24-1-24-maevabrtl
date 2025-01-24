c39800522e0eb270d875c719fd85535a6ea146de
Maya <maeva.berthail@student.adatechschool.fr>
2025-01-24 10:22:03 +0100

    afficher triangle droite working

--- script.js
+++ script.js
@@ -10,6 +10,37 @@ function afficherRectangle(hauteur, largeur) {
 		afficherEtoiles(largeur);
 }
 
-afficherRectangle(5, 5);
-afficherRectangle(2, 5);
-afficherRectangle(7, 3);
+function créerLigneDroite(n) {
+	let starStr = '\\';
+	if (n > 0) {
+		starStr = '*';
+		for (let i = 1; i < n; i++)
+			starStr += '*';
+		starStr += '\\';
+	}
+	return (starStr);
+}
+
+function afficherTriangleDroite(n) {
+	let lineStr;
+	for (let i = 0; i < n; i++) {
+		lineStr = créerLigneDroite(i);
+		console.log(lineStr);
+	}
+}
+
+afficherTriangleDroite(5);
+console.log(' ');
+afficherTriangleDroite(3);
+console.log(' ');
+afficherTriangleDroite(2);
+console.log(' ');
+afficherTriangleDroite(0);
+console.log(' ');
+afficherTriangleDroite(9);
+console.log(' ');
+afficherTriangleDroite(10);
+console.log(' ');
+afficherTriangleDroite(11);
+console.log(' ');
+afficherTriangleDroite(8);
