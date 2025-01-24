09cbfa0eb87a93cdd636f67ab40c8f880f09ec39
Maya <maeva.berthail@student.adatechschool.fr>
2025-01-24 10:39:51 +0100

    pointe sapin working

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-function afficherEtoiles(n) {
-	let starStr = '';
-	for (let i = 0; i < n; i++)
-		starStr += '*';
-	console.log(starStr);
-}
-
-function afficherRectangle(hauteur, largeur) {
-	for (let i = 0; i < hauteur; i++)
-		afficherEtoiles(largeur);
-}
-
-function créerLigneDroite(n) {
+function créerLigneDroite(longueur) {
 	let starStr = '\\';
-	if (n > 0) {
+	if (longueur > 0) {
 		starStr = '*';
-		for (let i = 1; i < n; i++)
+		for (let i = 1; i < longueur; i++)
 			starStr += '*';
 		starStr += '\\';
 	}
 	return (starStr);
 }
 
-function afficherTriangleDroite(n) {
-	let lineStr;
-	for (let i = 0; i < n; i++) {
-		lineStr = créerLigneDroite(i);
-		console.log(lineStr);
+function créerLigneGauche(longueurSapin, longueurTotale) {
+	let longueurEspace = longueurTotale - longueurSapin;
+	let line;
+	if (longueurSapin == longueurTotale)
+		line = '/';
+	else
+		line = ' ';
+	for (let i = 1; i < longueurTotale; i++) {
+		if (i < longueurEspace)
+			line += ' ';
+		else if (i == longueurEspace)
+			line += '/';
+		else
+			line += '*'		
+	}
+	return (line);
+}
+
+function créerPremièreLigne(hauteur) {
+	let line = ' '
+	for ( ; hauteur > 1; hauteur--)
+		line += ' ';
+	line += '+';
+	return (line);
+}
+
+function afficherPointeSapin(hauteur) {
+	console.log(créerPremièreLigne(hauteur));
+	let line;
+	for (let i = 1; i <= hauteur; i++) {
+		line = créerLigneGauche(i, hauteur) + '|' + créerLigneDroite(i - 1);
+		console.log(line);
 	}
 }
 
-afficherTriangleDroite(5);
-console.log(' ');
-afficherTriangleDroite(3);
-console.log(' ');
-afficherTriangleDroite(2);
-console.log(' ');
-afficherTriangleDroite(0);
-console.log(' ');
-afficherTriangleDroite(9);
-console.log(' ');
-afficherTriangleDroite(10);
-console.log(' ');
-afficherTriangleDroite(11);
-console.log(' ');
-afficherTriangleDroite(8);
+afficherPointeSapin(5);
